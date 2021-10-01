510840a187356b4f99bfb53c5d589c0fb391b4a5
uijunii <uijunii@uijuniiui-MacBookAir.local>
2021-10-01 13:50:39 +0900

    commit 10/10 13:51

--- App.js
+++ App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
 
 const App = () => {
-  
+
   const num = 0;
+  const num1 = 1;
 
   return (
     <SafeAreaView>
